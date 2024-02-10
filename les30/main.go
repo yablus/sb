c41fa16f9838e72652ebe6ed4fef7635824b61cb
Evgeny Lagunov <eelagunov@gmail.com>
2024-02-11 00:19:24 +0300

    less30

--- les30/main.go
+++ les30/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
+type service struct {
+	store map[string]string
+}
+
+func (s *service) Create(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		content, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		splittedContent := strings.Split(string(content), " ")
+		s.store[splittedContent[0]] = string(content)
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Пользователь создан:" + splittedContent[0]))
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func (s *service) GetAll(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		response := ""
+		for _, user := range s.store {
+			response += user + "\n"
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(response))
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+}
+
 func main() {
+
+	mux := http.NewServeMux()
+	/*
+		mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Write([]byte("Hello World!"))
+		})
+	*/
+	srv := service{make(map[string]string)}
+	mux.HandleFunc("/create", srv.Create)
+	mux.HandleFunc("/get", srv.GetAll)
+
+	http.ListenAndServe("localhost:8080", mux)
 
 }
