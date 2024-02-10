ed967b15baa40faa0b0aea084cae6783eb875d56
Evgeny Lagunov <eelagunov@gmail.com>
2024-02-11 01:20:32 +0300

    les30-3,4

--- les30/main.go
+++ les30/main.go
@@ -1,6 +1,84 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func (u *User) ToString() string {
+	//return fmt.Sprintf("Имя: %s, возраст: %d\n", u.Name, u.Age)
+	return fmt.Sprintf("name:%s,age:%d\n", u.Name, u.Age)
+}
+
+type service struct {
+	store map[string]*User
+}
+
+func (s *service) Create(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		content, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer r.Body.Close()
+
+		var u User
+		if err := json.Unmarshal(content, &u); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		s.store[u.Name] = &u
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Пользователь создан:" + u.Name))
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func (s *service) GetAll(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		response := ""
+		for _, user := range s.store {
+			response += user.ToString()
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(response))
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("Hello World!"))
+	})
+
+	srv := service{make(map[string]*User)}
+	mux.HandleFunc("/create", srv.Create)
+	mux.HandleFunc("/get", srv.GetAll)
+
+	http.ListenAndServe("localhost:8080", mux)
+
+}
+
+/*
+package main
+
+import (
 	"io"
 	"net/http"
 	"strings"
@@ -54,3 +132,4 @@ func main() {
 	http.ListenAndServe("localhost:8080", mux)
 
 }
+*/
